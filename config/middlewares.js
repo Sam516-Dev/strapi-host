13150930c8cbd99c8288f4ec75ad15c8dc1da243
Samuel Njoroge <samuelnjoroge3455@gmail.com>
2025-08-22 15:32:40 +0300

    middleware update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,34 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+  },
+  {
+    name: 'strapi::content-security-policy',
+    config: {
+      directives: {
+        'script-src': ["'self'", "'unsafe-inline'", 'blob:'],
+      },
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
